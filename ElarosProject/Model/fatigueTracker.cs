876abe83daf15d263910bc2bc805b233ee1eda77
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:20:17 +0000

    Add files via upload

---
+++ ElarosProject/Model/fatigueTracker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace CovidApp.Models
+{
+	public class fatigueTracker
+	{
+        public string date { get; set; }
+    
+		public ArrayList activities { get; set; }
+
+		public double fatigueLevel { get; set; }
+
+        public fatigueTracker(string entryDate,double level)
+		{
+
+			date = entryDate;
+			//activities = Activity;
+			fatigueLevel = level;
+
+		}
+	}
+}
+
