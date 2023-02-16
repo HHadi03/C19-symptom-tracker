f57f6ef9018fe5be8e10e49d655852446f60035b
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:12:33 +0000

    Add files via upload

---
+++ ElarosProject/ViewModel/FatigueTrackerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace CovidApp.ViewModels
+{
+    public class FatigueTrackerViewModel : BaseViewModel
+    {
+        public ObservableCollection<Models.fatigueTracker> logActivities { get; set; }
+        public string myDate { get; } = DateTime.Today.ToString("dd/MM/yyyy");
+
+       
+        public FatigueTrackerViewModel()
+        {
+            Title = "Fatigue Tracker";
+            logActivities.Add(new Models.fatigueTracker(myDate,));
+        }
+
+        
+    }
+}
