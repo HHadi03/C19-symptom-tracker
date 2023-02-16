35d67837ee0dc38b173a1d06745ddb34e068a7af
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:11:45 +0000

    Add files via upload

---
+++ ElarosProject/View/DiaryEntries.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CovidApp.Views
+{
+    public partial class DiaryEntries : ContentPage
+    {
+        public DiaryEntries()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModels.FatigueTrackerViewModel();
+           
+        }
+
+       
+
+    }
+}
+
