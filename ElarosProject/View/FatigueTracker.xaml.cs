46052a2b2776868d1dacb77e12ed7880ae49f7f9
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:10:44 +0000

    Add files via upload

---
+++ ElarosProject/View/FatigueTracker.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace CovidApp.Views
+{	
+	public partial class FatigueTracker : ContentPage
+	{
+       
+        public FatigueTracker ()
+		{
+            InitializeComponent();
+            BindingContext = new ViewModels.FatigueTrackerViewModel();
+        }
+
+		void addItem(object sender,EventArgs args)
+		{
+
+            var newActivity = new Entry { Placeholder = "Add Entry", PlaceholderColor = Color.LightGray, ClearButtonVisibility = ClearButtonVisibility.WhileEditing,FontFamily="myFont"  };
+            myLayout.Children.Insert(myLayout.Children.Count - 1, newActivity);
+        }
+
+        void submitActivities(object sender,EventArgs args)
+        {
+            
+        }
+    }
+}
+
