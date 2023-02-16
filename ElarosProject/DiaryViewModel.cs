c507da43cac7ac76258fcd530ec213c39380e9f7
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:12:57 +0000

    Add files via upload

---
+++ ElarosProject/DiaryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+namespace CovidApp.ViewModels
+
+{
+	public class DiaryViewModel : BaseViewModel
+	{
+		public ObservableCollection<Models.diaryEntry> allEntries { get; set; }
+		public DiaryViewModel()
+		{
+			Title = "My Diary";
+			allEntries = new ObservableCollection<Models.diaryEntry>();
+
+			allEntries.Add(new Models.diaryEntry("15/12/2022", "School Drop-off, Grocery Shopping", 5));
+            allEntries.Add(new Models.diaryEntry("16/12/2022", "Morning walk, interview", 6));
+        }
+	}
+}
+
