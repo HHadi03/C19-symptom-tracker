5d4aae05f0022c515a045530ecccd7c491c1448f
jrboothman <97126531+jrboothman@users.noreply.github.com>
2023-02-16 11:09:38 +0000

    goal setting ui and some backend

---
+++ ElarosProject/Model/GoalModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ElarosProject.Model
+{
+    public class GoalModel
+    {
+        public string Name { get; set; }
+        public string Symptom { get; set; }
+        public string SeverityLevel { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TargetDate { get; set; }
+
+        public GoalModel(string Name, string Symptom, string SeverityLevel, DateTime StartDate, DateTime TargetDate)
+        {
+            this.Name = Name;
+            this.Symptom = Symptom;
+            this.SeverityLevel = SeverityLevel;
+            this.StartDate = StartDate;
+            this.TargetDate = TargetDate;
+        }
+
+        public string GetName() { return Name; }
+        public string GetSymptom() { return Symptom; }
+        public string GetSeverityLevel() { return SeverityLevel; }
+        public DateTime GetStartDate() { return StartDate; }
+        public DateTime GetTargetDate() { return TargetDate; }
+    }
+}
---
+++ ElarosProject/View/NewGoal.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ElarosProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ElarosProject.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewGoal : ContentPage
+    {
+        public NewGoal()
+        {
+            InitializeComponent();
+        }
+
+        protected void SaveClick(object sender, EventArgs e, GoalModel Ngoal)
+        {
+            // checks if goal name is left blank
+            if (GoalName.Text == null) 
+            {
+                DisplayAlert("Error", "Goal name can't be left blank", "OK");
+                GoalName.Text = null;
+                return;
+            }
+
+            //code to create goal object
+            Ngoal.Name = GoalName.Text;
+            Ngoal.SeverityLevel = GoalSeverity.SelectedItem.ToString();
+            Ngoal.StartDate = DateTime.Now;
+            Ngoal.TargetDate = TargetDatePicker.Date;
+
+            // code to save goal to database
+
+            DisplayAlert("SUCCESS", "Goal saved", "OK");
+            this.Navigation.PushAsync(new Goals());
+        }
+    }
+}
