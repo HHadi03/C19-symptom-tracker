590c1578d014bc65cf8ca8e9e07135ed4b75d6e9
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-02-16 12:23:13 +0000

    Add files via upload

---
+++ ElarosProject/Model/diaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CovidApp.Models
+{
+	public class diaryEntry
+	{
+		public string date { get; set; }
+        public string activities { get; set; }
+        public int fatigueLevel{ get; set; }
+  
+        public diaryEntry(string Date,string Activities,int fatigue)
+		{
+			date = Date;
+			activities = Activities;
+			fatigueLevel = fatigue;
+		}
+	}
+}
+
