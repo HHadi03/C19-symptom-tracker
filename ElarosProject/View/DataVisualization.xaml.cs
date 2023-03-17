369305fb307ec5868aa12746ec6a810704357382
NSinnott02 <93921911+NSinnott02@users.noreply.github.com>
2023-03-17 14:12:22 +0000

    Altered data vis code so it is now in MVVM structure

--- ElarosProject/View/DataVisualization.xaml.cs
+++ ElarosProject/View/DataVisualization.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microcharts;
+﻿using ElarosProject.ViewModel;
+using Microcharts;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
@@ -14,108 +15,32 @@ namespace ElarosProject.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DataVisualization : ContentPage
     {
-        private readonly ChartEntry[] symptoms = new[]
-        {
-            new ChartEntry(10)
-            {
-                Label="Breathlessness",
-                ValueLabel="10",
-                Color = SKColor.Parse("#2F5CAF")
-            },
-            new ChartEntry(7)
-            {
-                Label="Cough",
-                ValueLabel="7",
-                Color = SKColor.Parse("#FDDDC7")
-            },
-            new ChartEntry(3)
-            {
-                Label="Swallowing",
-                ValueLabel="3",
-                Color = SKColor.Parse("#F7B176")
-            },
-            new ChartEntry(9)
-            {
-                Label="Fatigue",
-                ValueLabel="9",
-                Color = SKColor.Parse("#54A08C")
-            },
-            new ChartEntry(8)
-            {
-                Label="Continence",
-                ValueLabel = "8",
-                Color = SKColor.Parse("#7865B6")
-            },
-            new ChartEntry(4)
-            {
-                Label="Pain",
-                ValueLabel = "4",
-                Color = SKColor.Parse("#EDD182")
-            },
-            new ChartEntry(5)
-            {
-                Label="Cognition",
-                ValueLabel="5",
-                Color = SKColor.Parse("#2F5CAF")
-            },
-            new ChartEntry(2)
-            {
-                Label="Anxiety",
-                ValueLabel = "2",
-                Color = SKColor.Parse("#FDDDC7")
-            },
-        };
-
-        private readonly ChartEntry[] disabilities = new[]
-        {
-            new ChartEntry(7)
-            {
-                Label="Communication",
-                ValueLabel = "7",
-                Color = SKColor.Parse("#7865B6")
-            },
-            new ChartEntry(3)
-            {
-                Label="Mobility",
-                ValueLabel = "3",
-                Color = SKColor.Parse("#54A08C")
-            },
-            new ChartEntry(4)
-            {
-                Label="Personal-Care",
-                ValueLabel = "4",
-                Color = SKColor.Parse("#FDDDC7")
-            },
-            new ChartEntry(10)
-            {
-                Label="Other",
-                ValueLabel = "10",
-                Color = SKColor.Parse("#2F5CAF")
-            }
-        };
+        private readonly DataVisualizationViewModel _viewModel;
         public DataVisualization()
         {
             InitializeComponent();
-            chartViewSymptoms.Chart = new RadarChart { Entries = symptoms };
-            chartViewDisabilities.Chart = new RadarChart { Entries = disabilities };
+
+            _viewModel = new DataVisualizationViewModel();
+            chartViewSymptoms.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._disabilities) };
         }
 
         private void BarChartClicked(object sender, EventArgs e)
         {
-            chartViewSymptoms.Chart = new BarChart { Entries = symptoms };
-            chartViewDisabilities.Chart = new BarChart { Entries = disabilities };
+            chartViewSymptoms.Chart = new BarChart { Entries = _viewModel.GetBarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new BarChart { Entries = _viewModel.GetBarChartData(_viewModel._disabilities) };
         }
 
         private void RadarChartClicked(object sender, EventArgs e)
         {
-            chartViewSymptoms.Chart = new RadarChart { Entries = symptoms };
-            chartViewDisabilities.Chart = new RadarChart { Entries = disabilities };
+            chartViewSymptoms.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._disabilities) };
         }
 
         private void RadialChartClicked(object sender, EventArgs e)
         {
-            chartViewSymptoms.Chart = new RadialGaugeChart { Entries = symptoms };
-            chartViewDisabilities.Chart = new RadialGaugeChart { Entries = disabilities };
+            chartViewSymptoms.Chart = new RadialGaugeChart { Entries = _viewModel.GetRadialChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadialGaugeChart { Entries = _viewModel.GetRadialChartData(_viewModel._disabilities) };
         }
     }
 }
---
+++ ElarosProject/ViewModel/DataVisualizationVM.cs
@@ -0,0 +1,168 @@
+﻿using Microcharts;
+using SkiaSharp;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ElarosProject.ViewModel
+{
+    public class DataVisualizationViewModel : INotifyPropertyChanged
+    {
+        public ChartEntry[] _symptoms;
+        public ChartEntry[] _disabilities;
+        private Chart _symptomsChart;
+        private Chart _disabilitiesChart;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Chart SymptomsChart
+        {
+            get => _symptomsChart;
+            set => SetProperty(ref _symptomsChart, value);
+        }
+
+        public Chart DisabilitiesChart
+        {
+            get => _disabilitiesChart;
+            set => SetProperty(ref _disabilitiesChart, value);
+        }
+
+        public DataVisualizationViewModel()
+        {
+            _symptoms = new[]
+            {
+                new ChartEntry(10)
+                {
+                    Label="Breathlessness",
+                    ValueLabel="10",
+                    Color = SKColor.Parse("#2F5CAF")
+                },
+                new ChartEntry(7)
+                {
+                    Label="Cough",
+                    ValueLabel="7",
+                    Color = SKColor.Parse("#FDDDC7")
+                },
+                new ChartEntry(3)
+                {
+                    Label="Swallowing",
+                    ValueLabel="3",
+                    Color = SKColor.Parse("#F7B176")
+                },
+                new ChartEntry(9)
+                {
+                    Label="Fatigue",
+                    ValueLabel="9",
+                    Color = SKColor.Parse("#54A08C")
+                },
+                new ChartEntry(8)
+                {
+                    Label="Continence",
+                    ValueLabel = "8",
+                    Color = SKColor.Parse("#7865B6")
+                },
+                new ChartEntry(4)
+                {
+                    Label="Pain",
+                    ValueLabel = "4",
+                    Color = SKColor.Parse("#EDD182")
+                },
+                new ChartEntry(5)
+                {
+                    Label="Cognition",
+                    ValueLabel="5",
+                    Color = SKColor.Parse("#2F5CAF")
+                },
+                new ChartEntry(2)
+                {
+                    Label="Anxiety",
+                    ValueLabel = "2",
+                    Color = SKColor.Parse("#FDDDC7")
+                },
+            };
+
+            _disabilities = new[]
+            {
+                new ChartEntry(7)
+                {
+                    Label="Communication",
+                    ValueLabel = "7",
+                    Color = SKColor.Parse("#7865B6")
+                },
+                new ChartEntry(3)
+                {
+                    Label="Mobility",
+                    ValueLabel = "3",
+                    Color = SKColor.Parse("#54A08C")
+                },
+                new ChartEntry(4)
+                {
+                    Label="Personal-Care",
+                    ValueLabel = "4",
+                    Color = SKColor.Parse("#FDDDC7")
+                },
+                new ChartEntry(10)
+                {
+                    Label="Other",
+                    ValueLabel = "10",
+                    Color = SKColor.Parse("#2F5CAF")
+                }
+            };
+
+            ShowRadarCharts();
+        }
+
+        public void ShowBarCharts()
+        {
+            SymptomsChart = new BarChart { Entries = _symptoms };
+            DisabilitiesChart = new BarChart { Entries = _disabilities };
+        }
+
+        public void ShowRadarCharts()
+        {
+            SymptomsChart = new RadarChart { Entries = _symptoms };
+            DisabilitiesChart = new RadarChart { Entries = _disabilities };
+        }
+
+        public void ShowRadialCharts()
+        {
+            SymptomsChart = new RadialGaugeChart { Entries = _symptoms };
+            DisabilitiesChart = new RadialGaugeChart { Entries = _disabilities };
+        }
+
+        public ChartEntry[] GetRadarChartData(ChartEntry[] data)
+        {
+            return data;
+        }
+
+        public ChartEntry[] GetBarChartData(ChartEntry[] data)
+        {
+            return data;
+        }
+
+        public ChartEntry[] GetRadialChartData(ChartEntry[] data)
+        {
+            return data;
+        }
+
+
+        private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
