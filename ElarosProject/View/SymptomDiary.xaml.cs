00e7ae955a6c7e7946da86dc4c8334c54f9275ee
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-03-28 13:36:47 +0100

    Test upload
    Amendments required to Binding source (symptom and date)- testing db connection & UId

---
+++ ElarosProject/View/SymptomDiary.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ElarosProject.Model;
+using ElarosProject.ViewModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Xaml;
+
+namespace ElarosProject.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SymptomDiary : ContentPage
+    {
+        private LoginModel currentUser = Application.Current.Properties["currentUser"] as LoginModel;
+        private AssessmentVM _assessmentVM = Application.Current.Properties["_assessmentVM"] as AssessmentVM;
+        public LoginVM _loginVM = Application.Current.Properties["_loginVM"] as LoginVM;
+
+        public string uID;
+        public ObservableCollection<AssessmentModel> updatedSymptom;
+
+        databaseConnection connect = new databaseConnection();
+
+        public SymptomDiary()
+        {
+            InitializeComponent();
+            uID = currentUser.GetUserID();
+            updatedSymptom = _assessmentVM.getSymptomByUser(uID);
+            BindingContext = this; 
+        }
+
+        void updateSeverity(Object sender, EventArgs e)
+        {
+            sliderContainer.IsVisible = !sliderContainer.IsVisible; //if update button clicked, shows/hides slider depending on current state
+        }
+
+        void deleteSymptom(Object sender, EventArgs e)
+        {
+
+        }
+
+        void sliderChange(object sender, ValueChangedEventArgs e)
+        {
+
+            var Step = Math.Round(e.NewValue / 1);
+            slider.Value = Step;
+
+        }
+
+
+        void submitUpdate(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
+
+
