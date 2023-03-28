7e64a1345aa5fe1328d394dfa11852b92b678d55
jrboothman <97126531+jrboothman@users.noreply.github.com>
2023-03-28 10:15:14 +0100

    data vis stuff added

--- ElarosProject/View/DataVisualization.xaml.cs
+++ ElarosProject/View/DataVisualization.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ElarosProject.ViewModel;
+using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,32 @@ namespace ElarosProject.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DataVisualization : ContentPage
     {
+        private readonly DataVisualizationViewModel _viewModel;
         public DataVisualization()
         {
             InitializeComponent();
+
+            _viewModel = new DataVisualizationViewModel();
+            chartViewSymptoms.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._disabilities) };
+        }
+
+        private void BarChartClicked(object sender, EventArgs e)
+        {
+            chartViewSymptoms.Chart = new BarChart { Entries = _viewModel.GetBarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new BarChart { Entries = _viewModel.GetBarChartData(_viewModel._disabilities) };
+        }
+
+        private void RadarChartClicked(object sender, EventArgs e)
+        {
+            chartViewSymptoms.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadarChart { Entries = _viewModel.GetRadarChartData(_viewModel._disabilities) };
+        }
+
+        private void RadialChartClicked(object sender, EventArgs e)
+        {
+            chartViewSymptoms.Chart = new RadialGaugeChart { Entries = _viewModel.GetRadialChartData(_viewModel._symptoms) };
+            chartViewDisabilities.Chart = new RadialGaugeChart { Entries = _viewModel.GetRadialChartData(_viewModel._disabilities) };
         }
     }
 }
