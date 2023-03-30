9d3a5b34f219f5b95a95c75a5aee3f82ab71f380
Sumaiya <93351703+sumaiya-r@users.noreply.github.com>
2023-03-30 01:59:38 +0100

    Constructor overload to support symptom updates

---
+++ ElarosProject/Model/AssessmentModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElarosProject.Model
+{
+    public class AssessmentModel
+    {
+        public string User { get; set; }
+        public string Symptom { get; set; }
+        public int Severity { get; set; }
+        public string DateLogged { get; set; }
+        public static int completedPages { get; set; } = 0; 
+
+        public string updatedSeverity{ get; set; } //Isolating updating severity to ensure it doesn't interfere with dashboard carousel
+
+        public AssessmentModel(string user, string symptom, int severity, string dateLogged)
+        {
+            User = user;
+            Symptom = symptom;
+            Severity = severity;
+            DateLogged = dateLogged;
+        
+        }
+
+        public AssessmentModel(string user, string symptom, int severity, string dateLogged, string UpdatedSeverity)
+        {
+            User = user;
+            Symptom = symptom;
+            Severity = severity;
+            DateLogged = dateLogged;
+            updatedSeverity = UpdatedSeverity; //added in 
+        }
+
+        public AssessmentModel() { }
+    }
+}
